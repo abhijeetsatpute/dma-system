a705ce731f8c2d508538626e404ef4ab349aa7e9
abhijeetsatpute <abhijeetsatpute98@gmail.com>
2025-03-03 13:41:24 +0530

    chore: added @nestjs/swagger setup with API documentation at /api-docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger UI
+  const config = new DocumentBuilder()
+    .setTitle('Document Management QnA')
+    .setDescription('Document Management QnA API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, document);
+
   await app.listen(PORT ?? 3000);
 }
 bootstrap();
